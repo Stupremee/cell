f73bbea47c4d1f705dab87f0dade56de698aa3cc
Justus K <justus.k@protonmail.com>
2020-08-09 01:21:17 +0200

    We dont use rowan here

--- xtask/src/codegen.rs
+++
@@ -1,9 +0,0 @@
-//! Code generation for the AST, Syntax Kinds and Syntax Nodes.
-
-mod syntax;
-
-pub use syntax::generate_syntax;
-
-const SYNTAX_KINDS: &str = "crates/cell_syntax/src/syntax_kind/generated.rs";
-const AST_NODES: &str = "crates/cell_syntax/src/ast/generated/nodes.rs";
-const AST_TOKENS: &str = "crates/cell_syntax/src/ast/generated/tokens.rs";
